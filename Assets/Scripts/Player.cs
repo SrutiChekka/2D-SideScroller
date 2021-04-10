da8fd9d4a5ed5f106f463b3b0eb1b2e50bf1858d
SrutiChekka <SrutiChekka@users.noreply.github.com>
2021-04-10 17:44:02 +0530

    Add Project

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpSpeed = 8f;
+    private float movement = 0f;
+    private Rigidbody2D rigidBody;
+    private Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+        if (movement > 0f)
+        {
+            rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
+            anim.SetBool("IsRunning", true);
+        }
+        else if (movement < 0f)
+        {
+            rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
+            anim.SetBool("IsRunning", true);
+        }
+        else
+        {
+            rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
+            anim.SetBool("IsRunning", false);
+            anim.SetBool("IsJumping", false);
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeed);
+            anim.SetBool("IsJumping", true);
+        }
+    }
+}
